057cb3bec6ef960dd1cdc148c6f7d838df28e254
Denis <korbakovd@gmail.com>
2025-03-16 13:56:13 +0300

    create new middleware

---
+++ app/Http/Middleware/Role.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\Request;
+use Closure;
+
+class Role
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param string ...$roles
+     */
+    public function handle(Request $request, Closure $next, ...$roles): Response
+    {
+        if (! Auth::check()) {
+            return redirect('login');
+        }
+
+        $user = Auth::user();
+
+        foreach ($roles as $role) {
+            if ($user?->hasRole($role)) {
+                return $next($request);
+            }
+        }
+
+        return redirect('login');
+    }
+}
