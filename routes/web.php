bfcc0fc9ecbb38cd106fe5724dca94b4426dcbb2
Denis <korbakovd@gmail.com>
2025-03-25 23:30:41 +0300

    delete extra logic

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-require __DIR__ . '/auth.php';
-
 Route::prefix('api')->group(base_path('routes/api.php'));
 
 Route::get('/', static function () {
