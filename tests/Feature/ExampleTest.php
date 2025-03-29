7902bec53f5a76fa9cd151b7b654122cbd16336c
Denis <korbakovd@gmail.com>
2025-03-29 17:36:33 +0300

    delete extra tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 it('returns a successful response', function (): void {
-    $response = $this->get('/');
+    $response = $this->get('/up');
 
     $response->assertStatus(200);
 });
