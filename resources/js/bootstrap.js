696b8a85e370d85c0e98fde536d212687674bd04
Denis <korbakovd@gmail.com>
2025-04-05 15:29:14 +0300

    delete extra file

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,5 +8,3 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allow your team to quickly build robust real-time web applications.
  */
-
-import './echo';
