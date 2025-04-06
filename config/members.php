12f899cea3df13e173f9a82614a759dc80dceb9b
Denis <korbakovd@gmail.com>
2025-04-06 09:06:38 +0300

    update seed

--- config/members.php
+++ config/members.php
@@ -16,51 +16,51 @@ return [
     [
         'number' => 2,
         'color' => '#0000ff',
-        'reaction_time' => 0.25,
-        'boost' => 3.8,
-        'max_speed' => 10.5,
-        'speed_loss' => 0.22,
-        'stability_from' => 1.5,
-        'stability_to' => 2.5,
+        'reaction_time' => 0.12,
+        'boost' => 4.5,
+        'max_speed' => 11.2,
+        'speed_loss' => 0.18,
+        'stability_from' => 2.5,
+        'stability_to' => 3.5
     ],
     [
         'number' => 3,
         'color' => '#FFA500',
-        'reaction_time' => 0.18,
-        'boost' => 4.2,
-        'max_speed' => 9.2,
-        'speed_loss' => 0.15,
-        'stability_from' => 3.5,
-        'stability_to' => 4.5
+        'reaction_time' => 0.12,
+        'boost' => 4.5,
+        'max_speed' => 11.2,
+        'speed_loss' => 0.18,
+        'stability_from' => 2.5,
+        'stability_to' => 3.5
     ],
     [
         'number' => 4,
         'color' => '#00ff00',
-        'reaction_time' => 0.3,
-        'boost' => 3.5,
-        'max_speed' => 12.0,
-        'speed_loss' => 0.3,
-        'stability_from' => 0.5,
-        'stability_to' => 1.5
+        'reaction_time' => 0.12,
+        'boost' => 4.5,
+        'max_speed' => 11.2,
+        'speed_loss' => 0.18,
+        'stability_from' => 2.5,
+        'stability_to' => 3.5
     ],
     [
         'number' => 5,
         'color' => '#7F00FF',
-        'reaction_time' => 0.15,
-        'boost' => 4.0,
-        'max_speed' => 10.0,
-        'speed_loss' => 0.2,
-        'stability_from' => 4.5,
-        'stability_to' => 5
+        'reaction_time' => 0.12,
+        'boost' => 4.5,
+        'max_speed' => 11.2,
+        'speed_loss' => 0.18,
+        'stability_from' => 2.5,
+        'stability_to' => 3.5
     ],
     [
         'number' => 6,
         'color' => '#000000',
-        'reaction_time' => 0.22,
-        'boost' => 3.9,
-        'max_speed' => 11.0,
-        'speed_loss' => 0.25,
-        'stability_from' => 1.5,
-        'stability_to' => 2.5
+        'reaction_time' => 0.12,
+        'boost' => 4.5,
+        'max_speed' => 11.2,
+        'speed_loss' => 0.18,
+        'stability_from' => 2.5,
+        'stability_to' => 3.5
     ]
 ];
